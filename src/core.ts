8f2d302dd0f97367fd9337e692b4dd5d60f20443
hocgin <hocgin@gmail.com>
2022-10-28 23:53:23 +0800

    [新增功能](main): update

--- src/core.ts
+++ src/core.ts
@@ -5,25 +5,6 @@ const {owner} = github.context.repo;
 const {repo} = github.context.repo;
 const octokit = github.getOctokit(process.env.GITHUB_TOKEN!);
 
-export function run(input: Inputs): Outputs {
-    if (input.limit_tags > 0) {
-        let result = listAllTags(input.limit_tags);
-        result.forEach((name) => {
-            debugPrintf(`删除 tag.name=${name}`)
-            octokit.git.deleteRef({owner, repo, ref: `tags/${name}`});
-        });
-    }
-
-    if (input.limit_release > 0) {
-        let result = listAllReleases(input.limit_release);
-        result.forEach((id) => {
-            debugPrintf(`删除 release.id=${id}`)
-            octokit.repos.deleteRelease({owner, repo, release_id: id});
-        });
-    }
-    return {};
-}
-
 function listAllTags(limit: number) {
     let page = 1;
     let result: string[] = [];
@@ -56,4 +37,25 @@ function listAllReleases(limit: number) {
 
 function listAllErrorWorkflowLog(limit: number) {
     // octokit.actions.listWorkflowRuns()
+}
+
+export function run(input: Inputs): Outputs {
+    if (input.limit_tags > 0) {
+        debugPrintf(`正在处理 input.limit_tags`, input.limit_tags)
+        let result = listAllTags(input.limit_tags);
+        result.forEach((name) => {
+            debugPrintf(`删除 tag.name=${name}`)
+            octokit.git.deleteRef({owner, repo, ref: `tags/${name}`});
+        });
+    }
+
+    if (input.limit_release > 0) {
+        debugPrintf(`正在处理 input.limit_release`, input.limit_release)
+        let result = listAllReleases(input.limit_release);
+        result.forEach((id) => {
+            debugPrintf(`删除 release.id=${id}`)
+            octokit.repos.deleteRelease({owner, repo, release_id: id});
+        });
+    }
+    return {};
 }
