a72bc87a216775eee4bacc00978d6e03e185b365
hocgin <hocgin@gmail.com>
2022-10-28 22:55:47 +0800

    [新增功能](main): update

--- src/core.ts
+++ src/core.ts
@@ -1,40 +1,45 @@
 import * as github from '@actions/github';
-import {debugPrintf, Inputs, Outputs} from "./main";
-import {getOctokit} from "@actions/github";
+import {Inputs, Outputs} from "./main";
 
 const {owner} = github.context.repo;
 const {repo} = github.context.repo;
 const octokit = github.getOctokit(process.env.GITHUB_TOKEN!);
 
 export function run(input: Inputs): Outputs {
-    let page = 2;
     if (input.limit_tags > 0) {
-        let result = [];
-        do {
-            octokit.repos.listTags({owner, repo, page: page++, per_page: input.limit_tags}).then(({data}) => {
-                result = data;
-                debugPrintf('listTags', result);
-                for (let tag of result) {
-                    debugPrintf(`delete tag name = ${tag.name}`);
-                    octokit.git.deleteRef({owner, repo, ref: `tags/${tag.name}`});
-                }
-            });
-        } while (result.length !== input.limit_tags)
+        listAllTags(input.limit_tags).forEach(({name}) => {
+            octokit.git.deleteRef({owner, repo, ref: `tags/${name}`});
+        });
     }
 
-    page = 2;
     if (input.limit_release > 0) {
-        let result = [];
-        do {
-            octokit.repos.listReleases({owner, repo, page: page++, per_page: input.limit_release}).then(({data}) => {
-                result = data;
-                debugPrintf('listReleases', result);
-                for (let release of result) {
-                    debugPrintf(`delete release.id = ${release.id}`);
-                    octokit.repos.deleteRelease({owner, repo, release_id: release.id});
-                }
-            });
-        } while (result.length !== input.limit_release)
+        listAllReleases(input.limit_release).forEach(({id}) => {
+            octokit.repos.deleteRelease({owner, repo, release_id: id});
+        });
     }
     return {};
+}
+
+function listAllTags(limit: number) {
+    let page = 1;
+    let result: any[] = [];
+    let perPage = 200;
+    do {
+        octokit.repos.listTags({owner, repo, page: page++, per_page: perPage}).then(({data}) => {
+            result.push(data);
+        });
+    } while (result.length !== perPage)
+    return result.slice(Math.min(limit, result.length), result.length);
+}
+
+function listAllReleases(limit: number) {
+    let page = 1;
+    let result: any[] = [];
+    let perPage = 200;
+    do {
+        octokit.repos.listReleases({owner, repo, page: page++, per_page: perPage}).then(({data}) => {
+            result.push(data);
+        });
+    } while (result.length !== perPage)
+    return result.slice(Math.min(limit, result.length), result.length);
 }
